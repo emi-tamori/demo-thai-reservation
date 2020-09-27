a8ead5904f2a23132325390619249836e2255c63
user <user@rrcs-172-254-99-37.nyc.biz.rr.com>
2020-09-27 10:06:48 +0900

    yoyaku suru

--- index.js
+++ index.js
@@ -88,9 +88,16 @@ const handleMessageEvent = async (ev) => {
     console.log('ev:',ev);
     const profile = await client.getProfile(ev.source.userId);
     const text = (ev.message.type === 'text') ? ev.message.text : '';
-    
-    return client.replyMessage(ev.replyToken,{
-        "type":"text",
-        "text":`${profile.displayName}さん、今${text}って言いました？`
-    });
+
+    if(text === '予約する'){
+        return client.replyMessage(ev.replyToken,{
+            "type":"text",
+            "text":"かしこまりました。次回予約ですね。メニューは・・・"
+        });
+    }else{
+        return client.replyMessage(ev.replyToken,{
+            "type":"text",
+            "text":`${profile.displayName}さん、今${text}って言いました？`
+        });
+    }
 }
