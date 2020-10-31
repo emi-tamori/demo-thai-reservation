d8298e9b0b95883066cd72f63f18db062a8ddc35
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-10-31 16:40:46 +0900

    table create

--- public/js/users.js
+++ public/js/users.js
@@ -1,5 +1,7 @@
 (()=>{
     const API_URL = 'https://linebot-reservation.herokuapp.com/api/';
+    const HEADERS = ['ID','名前','登録日','Cut','Shampoo','Color','Spa','次回予約'];
+    const CLASSES = ['row-id','row-name','row-resist','row-cut','row-shampoo','row-color','row-spa','row-nextrev'];
 
     window.addEventListener('load',()=>{
         fetchData();
@@ -8,12 +10,89 @@
     const fetchData = async () => {
         try{
             const response = await fetch(API_URL);
-            console.log('response:',response);
-            const data = await response.json();
-            console.log('data:',data);
+            if(response.ok){
+                const data = await response.json();
+                createTable(data);
+            }else{
+                alert('HTTPレスポンスエラーです')
+            }
         }catch(error){
             alert('データ読み込み失敗です');
         }
+    }
+
+    const createTable = (data) => {
+
+        // data.usersを２次元配列の形にする
+        const usersData = [];
+        data.users.forEach(usersObj=>{
+
+            // 現在時刻のタイムスタンプ取得
+            const now = new Data().getTime();
+            console.log('now:',now);
+
+            // data.reservationsからdata.usersのline_uidが一致するもの、かつ現在時刻より先の予約データのみを抽出
+            const revData = data.reservations.filter(revObj1=>{
+                return usersObj.line_uid === revObj1.line_uid;
+            }).filter(revObj2=>{
+                return parseInt(revObj2.starttime) > now;
+            });
+
+            // revData.starttimeを日時文字列へ変換する
+            const nextReservationDate = getDate(parseInt(revData.starttime));
+
+            // usersObj.timestampを日時文字列へ変換する
+            const resistrationDate = getDate(parseInt(usersObj.timestamp));
+
+            // usersData配列へ配列を格納
+            usersData.push([
+                usersObj.id,
+                usersObj.display_name,
+                resistrationDate,
+                usersObj.cuttime,
+                usersObj.shampootime,
+                usersObj.colortime,
+                usersObj.spatime,
+                nextReservationDate
+            ]);
+        });
+
+        // 次回予約日を計算し、usersDataへpushする
+        const l = usersData.length+1;  //表題の分＋１している
+
+        // テーブル要素の生成
+        const table = document.createElement('table');
+        table.setAttribute('id','usersTable');
+
+        for(let i=0;i<l;i++){
+            //tr要素の挿入
+            const tr = table.insertRow(-1);
+
+            HEADERS.forEach((value,index)=>{
+                if(i===0){
+                    // 最初の行は表題（th）とする
+                    const th = document.createElement('th');
+                    th.setAttribute('class',`uTitles ${CLASSES[index]}`);
+                    th.innerHTML = value;
+                    tr.appendChild(th);
+                }else{
+                    // ２行目以降はユーザーデータを格納する要素とする
+                    const td = document.createElement('td');
+                    td.setAttribute('class',`uElements ${CLASSES[index]}`);
+                    td.innerHTML = usersData[index];
+                }
+            });
+        }
+    }
+
+    const getDate = (timestamp) => {
+        const date = new Date(timestamp);
+        const y = date.getFullYear();
+        const m = ("0" + (date.getMonth()+1)).slice(-2);
+        const d = ("0" + date.getDate()).slice(-2);
+        const h = ("0" + date.getHours()).slice(-2);
+        const i = ("0" + date.getMinutes()).slice(-2);
+        return `${y}/${m}/${d} ${h}:${i}`;
     }
 
 })();
