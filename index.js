9048ef73733a3c7d6e51f5ba8f73d66bbca89da8
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-02 11:35:26 +0900

    multipart

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const path = require('path');
 const { Client } = require('pg');
 const router = require('./routers/index');
 const apiRouter = require('./routers/api');
+const multipart = require('connect-multiparty');
 
 const PORT = process.env.PORT || 5000
 
@@ -51,6 +52,7 @@ connection.query(create_userTable)
 
 app
     .use(express.static(path.join(__dirname,'public')))
+    .use(multipart())
     .use('/',router)
     .use('/api',apiRouter)
     .post('/hook',line.middleware(config),(req,res)=> lineBot(req,res))
