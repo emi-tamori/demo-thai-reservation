c3498f410018ffc0f43fc7769568dfb662d82359
user <user@rrcs-172-254-99-48.nyc.biz.rr.com>
2020-11-15 20:52:34 +0900

    reservedArray

--- index.js
+++ index.js
@@ -885,10 +885,10 @@ const checkReservable = (ev,menu,date) => {
           if(array[0]<ts10 && (array[1]>ts10 && array[1]<ts11)){
             reservedArray10.push(array.push(0));
           }
-          else if((array[0]>ts10 && array[0]<ts11) && array[1]>ts11){
+          else if((array[0]>=ts10 && array[0]<ts11) && array[1]>ts11){
             reservedArray10.push(array.push(1));
           }
-          else if((array[0]>ts10 && array[0]<ts11) && (array[1]>array[0] && array[1]<ts11)){
+          else if((array[0]>=ts10 && array[0]<ts11) && (array[1]>array[0] && array[1]<=ts11)){
             reservedArray10.push(array.push(2));
           }
           else if(array[0]<ts10 && array[1]>ts11){
