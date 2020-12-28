4ef0dcc5eb7fd5732ea322e4fedf97a811f68dc0
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2020-12-28 10:54:15 +0900

    json

--- public/js/staffs.js
+++ public/js/staffs.js
@@ -94,7 +94,7 @@
     postShiftButton.setAttribute('class','btn btn-primary post-button');
     postShiftButton.value = 'シフトデータ送信';
     postShiftButton.addEventListener('click',()=>{
-      const jsonData = JSON.stringify(data[0]);
+      const jsonData = JSON.stringify(data);
       console.log('jsondata',jsonData);
       // const formData = getFormData(data);
       // console.log('Formdata:',...formData.entries());
