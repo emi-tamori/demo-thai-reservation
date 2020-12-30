34a7ae1b301e9208e5e495d95b78f199491a1770
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2020-12-30 10:16:41 +0900

    reservation element

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -145,15 +145,16 @@
         }else{
           const startPoint3 = startPoint2 + ONEDAY*(j-1);
           td.setAttribute('class','table-element');
-
+          let text = ''
           //この時間帯に予約データがあるか確認し、あれば、td内に表示
           props.reservationsData.forEach(array=>{
             array.forEach(reservationInfo=>{
               if(reservationInfo.starttime>=startPoint3 && reservationInfo.starttime<(startPoint3+ONEHOUR)){
-                td.innerHTML = reservationInfo.staff;
+                text += reservationInfo.staff+',';
               }
             });
           });
+          td.innerHTML = text;
           trElement.appendChild(td);
         }
       }
