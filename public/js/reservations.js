949316ab69f991e2fc8df41eab545f4bf11d690e
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2020-12-30 13:46:57 +0900

    card

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -228,7 +228,315 @@
   }
 
   const createReservationCard = (info) =>{
-    console.log('data clicked!!');
+
+    //グリッドシステム
+    const divRow = document.createElement('div');
+    divRow.setAttribute('class','col-sm-6');
+    
+    // カード本体の定義
+    const divCard = document.createElement('div');
+    divCard.setAttribute('class','card text-white bg-primary card-reservation');
+    
+    //カード内文字列（テスト）
+    const p = document.createElement('p');
+    p.textContent = 'testだよ';
+    divCard.appendChild(p);
+    divRow.appendChild(divCard);
+
+    document.body.appendChild(divRow);
+
+    // カードヘッダーの定義
+    // const divHeader = document.createElement('div');
+    // divHeader.setAttribute('class','card-header');
+    // divHeader.innerHTML = `お客さまID:${userDataArray[0]}`;
+    // divCard.appendChild(divHeader);
+
+    // カードボディの定義
+    const divBody = document.createElement('div');
+    divBody.setAttribute('class','card-body');
+
+    // // form要素の生成
+    // const formElement = document.createElement('form');
+    // formElement.setAttribute('id','userForm');
+    // formElement.setAttribute('name','userInfo');
+    // formElement.setAttribute('method','post');
+
+    // // 名前入力フォームの生成
+    // const div_form_name = document.createElement('div');
+    // div_form_name.setAttribute('class','form-group');
+
+    // const label_name = document.createElement('label');
+    // label_name.setAttribute('class','label_user');
+    // label_name.innerHTML = '名前';
+    // div_form_name.appendChild(label_name);
+
+    // const input_name = document.createElement('input');
+    // input_name.setAttribute('type','text');
+    // input_name.setAttribute('class','form-control name-input');
+    // input_name.setAttribute('name','name');
+    // input_name.value = userDataArray[1];
+    // input_name.disabled = true;
+    // div_form_name.appendChild(input_name);
+
+    // formElement.appendChild(div_form_name);
+
+    // // カット時間入力フォームの生成
+    // const div_form_cut = document.createElement('div');
+    // div_form_cut.setAttribute('class','form-group inline-block menu-time');
+
+    // const label_cut = document.createElement('label');
+    // label_cut.setAttribute('class','label_user');
+    // label_cut.innerHTML = 'Cut';
+    // div_form_cut.appendChild(label_cut);
+
+    // const input_cut = document.createElement('input');
+    // input_cut.setAttribute('type','text');
+    // input_cut.setAttribute('class','form-control time-input');
+    // input_cut.setAttribute('name','cuttime');
+    // input_cut.value = userDataArray[3];
+    // input_cut.disabled = true;
+    // div_form_cut.appendChild(input_cut);
+
+    // formElement.appendChild(div_form_cut);
+
+    // // シャンプー時間の入力フォーム生成
+    // const div_form_shampoo = document.createElement('div');
+    // div_form_shampoo.setAttribute('class','form-group inline-block');
+
+    // const label_shampoo = document.createElement('label');
+    // label_shampoo.setAttribute('class','label_user');
+    // label_shampoo.innerHTML = 'Shampoo';
+    // div_form_shampoo.appendChild(label_shampoo);
+
+    // const input_shampoo = document.createElement('input');
+    // input_shampoo.setAttribute('type','text');
+    // input_shampoo.setAttribute('class','form-control time-input');
+    // input_shampoo.setAttribute('name','shampootime');
+    // input_shampoo.value = userDataArray[4];
+    // input_shampoo.disabled = true;
+    // div_form_shampoo.appendChild(input_shampoo);
+
+    // formElement.appendChild(div_form_shampoo);
+
+    // // カラーリング時間の入力フォーム生成
+    // const div_form_color = document.createElement('div');
+    // div_form_color.setAttribute('class','form-group inline-block menu-time');
+
+    // const label_color = document.createElement('label');
+    // label_color.setAttribute('class','label_user');
+    // label_color.innerHTML = 'Color';
+    // div_form_color.appendChild(label_color);
+
+    // const input_color = document.createElement('input');
+    // input_color.setAttribute('type','text');
+    // input_color.setAttribute('class','form-control time-input');
+    // input_color.setAttribute('name','colortime');
+    // input_color.value = userDataArray[5];
+    // input_color.disabled = true;
+    // div_form_color.appendChild(input_color);
+
+    // formElement.appendChild(div_form_color);
+
+    // // ヘッドスパ時間の入力フォーム生成
+    // const div_form_spa = document.createElement('div');
+    // div_form_spa.setAttribute('class','form-group inline-block');
+
+    // const label_spa = document.createElement('label');
+    // label_spa.setAttribute('class','label_user');
+    // label_spa.innerHTML = 'Spa';
+    // div_form_spa.appendChild(label_spa);
+
+    // const input_spa = document.createElement('input');
+    // input_spa.setAttribute('type','text');
+    // input_spa.setAttribute('class','form-control time-input');
+    // input_spa.setAttribute('name','spatime');
+    // input_spa.value = userDataArray[6];
+    // input_spa.disabled = true;
+    // div_form_spa.appendChild(input_spa);
+
+    // formElement.appendChild(div_form_spa);
+
+    // // 子要素の親要素へのappendChild
+    // divBody.appendChild(formElement);
+    // divCard.appendChild(divBody);
+
+    // // ボタン要素の作成
+    // const divButton = document.createElement('div');
+    // divButton.setAttribute('id','usercard-button-area');
+
+    // //編集ボタンの作成
+    // const editButton = document.createElement('input');
+    // editButton.setAttribute('class','btn btn-warning card-button');
+    // editButton.value = '編集';
+    // editButton.type = 'button';
+
+    // //編集ボタンクリック時の動作
+    // editButton.addEventListener('click',()=>{
+
+    //     //formのactionを設定　paramとしてidをつける
+    //     formElement.setAttribute('action',`api/users/${userDataArray[0]}`);
+
+    //     //各インプットの入力をできるようにする
+    //     input_name.disabled = false;
+    //     input_cut.disabled = false;
+    //     input_shampoo.disabled = false;
+    //     input_color.disabled = false;
+    //     input_spa.disabled = false;
+
+    //     //送信ボタンの生成
+    //     const sendButton = document.createElement('input');
+    //     sendButton.value = '送信';
+    //     sendButton.type = 'button';
+    //     sendButton.setAttribute('class','btn btn-warning card-button');
+
+    //     //sendButtonクリック時の処理
+    //     sendButton.addEventListener('click',(e)=>{
+    //         e.preventDefault();
+    //         if(!isNaN(document.userInfo.cuttime.value) && !isNaN(document.userInfo.shampootime.value) && !isNaN(document.userInfo.colortime.value) && !isNaN(document.userInfo.spatime.value)){
+    //             const data = new FormData(formElement);
+    //             console.log('FormData:',...data.entries());
+                
+    //             fetch(`/api/users/${userDataArray[0]}`,{
+    //                 method:'POST',
+    //                 body:data,
+    //                 creadentials:'same-origin'
+    //             })
+    //             .then(response=>{
+    //                 console.log('response:',response);
+    //                 if(response.ok){
+    //                     response.text()
+    //                         .then(text=>{
+    //                             alert(`${text}`);
+    //                             document.location.reload();
+    //                         })
+    //                         .catch(e=>console.log(e));
+    //                 }else{
+    //                     alert('HTTPレスポンスエラーです')
+    //                 }
+    //             })
+    //             .catch(e=>{
+    //                 throw e;
+    //             });
+    //         }else{
+    //             alert('時間は半角数値を入力してください。');
+    //         }
+    //     });
+
+    //     divButton.appendChild(sendButton);
+
+    //     //編集ボタンと削除ボタンを消す
+    //     deleteButton.style.display = 'none';
+    //     editButton.style.display = 'none';
+    // });
+
+    // divButton.appendChild(editButton);
+
+    // //削除ボタンの作成
+    // const deleteButton = document.createElement('input');
+    // deleteButton.setAttribute('class','btn btn-danger card-button');
+    // deleteButton.value = '削除';
+    // deleteButton.type = 'button';
+
+    // deleteButton.addEventListener('click',()=>{
+    //     // 処理を書く
+    // });
+
+    // divButton.appendChild(deleteButton);
+    // divCard.appendChild(divButton);
+
+    // //フッターの作成
+    // const divFooter = document.createElement('div');
+    // divFooter.setAttribute('class','card-footer text-center');
+    // divFooter.setAttribute('id','close-form');
+    // const closeButton = document.createElement('a');
+    // closeButton.setAttribute('class','closeButton');
+    // closeButton.textContent = '閉じる';
+    // divFooter.addEventListener('click',()=>{
+    //     divCard.style.display = 'none';
+    // });
+    // divFooter.appendChild(closeButton);
+
+    // divCard.appendChild(divFooter);
+
+    // マウスイベント
+    // divHeader.onmousedown = (e) =>{
+
+    //     let shiftX = e.clientX - divCard.getBoundingClientRect().left;
+    //     let shiftY = e.clientY - divCard.getBoundingClientRect().top;
+
+    //     const moveAt = (pageX,pageY) => {
+    //         if(pageX-shiftX>=0){
+    //             divCard.style.left = pageX - shiftX + 'px';
+    //         }else{
+    //             divCard.style.left = 0 + 'px';
+    //         }
+
+    //         if(pageY-shiftY>=0){
+    //             divCard.style.top = pageY - shiftY + 'px';
+    //         }else{
+    //             divCard.style.top = 0;
+    //         }
+    //     }
+
+    //     moveAt(e.pageX,e.pageY);
+
+    //     const onMouseMove = (e) => {
+    //         moveAt(e.pageX,e.pageY);
+    //     }
+
+    //     document.addEventListener('mousemove',onMouseMove);
+
+    //     divHeader.onmouseup = () => {
+    //         document.removeEventListener('mousemove',onMouseMove);
+    //         divHeader.onmouseup = null;
+    //     }
+
+    //     divHeader.onmouseleave = () => {
+    //         document.removeEventListener('mousemove',onMouseMove);
+    //         divHeader.onmouseleave = null;
+    //     }
+    // }
+
+    // // タッチイベント
+    // divHeader.ontouchstart = (event) =>{
+
+    //     const e = event.changedTouches[0];
+
+    //     let shiftX = e.clientX - divCard.getBoundingClientRect().left;
+    //     let shiftY = e.clientY - divCard.getBoundingClientRect().top;
+
+    //     const moveAt = (pageX,pageY) => {
+    //         if(pageX-shiftX>=0){
+    //             divCard.style.left = pageX - shiftX + 'px';
+    //         }else{
+    //             divCard.style.left = 0 + 'px';
+    //         }
+
+    //         if(pageY-shiftY>=0){
+    //             divCard.style.top = pageY - shiftY + 'px';
+    //         }else{
+    //             divCard.style.top = 0;
+    //         }
+    //     }
+
+    //     moveAt(e.pageX,e.pageY);
+
+    //     const onMouseMove = (event) => {
+    //         const e = event.changedTouches[0];
+    //         moveAt(e.pageX,e.pageY);
+    //     }
+
+    //     document.addEventListener('touchmove',onMouseMove);
+
+    //     divHeader.ontouchend = () => {
+    //         document.removeEventListener('touchmove',onMouseMove);
+    //         divHeader.ontouchend = null;
+    //     }
+    // }
+
+    // divHeader.ondragstart = () => {
+    //     return false;
+    // }
   }
 
 })();
