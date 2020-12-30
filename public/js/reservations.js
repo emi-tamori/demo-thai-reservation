abf4d73804da9e67e1c3305dd47deb37dff2daed
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2020-12-30 10:05:20 +0900

    reservation element

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -147,7 +147,7 @@
           td.setAttribute('class','table-element');
 
           //この時間帯に予約データがあるか確認し、あれば、td内に表示
-          reservationsData.forEach(array=>{
+          props.reservationsData.forEach(array=>{
             array.forEach(reservationInfo=>{
               if(reservationInfo.starttime>=startPoint3 && reservationInfo.starttime<(startPoint3+ONEHOUR)){
                 td.innerHTML = reservationInfo.staff;
