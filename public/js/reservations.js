790ef68b6d4fc2aa2c2c8268359fddc05010d371
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2020-12-31 15:13:58 +0900

    adjustment

--- public/js/reservations.js
+++ public/js/reservations.js
@@ -259,7 +259,7 @@
 
     //グリッドシステム
     const divRow = document.createElement('div');
-    divRow.setAttribute('class','col-sm-4 card-reservation');
+    divRow.setAttribute('class','col-md-4 card-reservation');
     divRow.style.top = '20vw';
     
     // カード本体の定義
