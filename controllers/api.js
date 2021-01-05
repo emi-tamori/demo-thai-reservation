c2281cc95cf727f67f7fa5be0924ac891c53d21c
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-05 23:29:16 +0900

    final

--- controllers/api.js
+++ controllers/api.js
@@ -4,7 +4,7 @@ module.exports = {
     getData: (req,res) => {
         Data.findData()
             .then(data=>{
-                console.log('data in controller:',data);
+                // console.log('data in controller:',data);
                 res.status(200).json(data);
             })
             .catch(e=>console.log(e));
