9245d98c47bda5e97aa45ae5c5cad21b5deabdc3
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-05 14:32:18 +0900

    test

--- public/js/staffs.js
+++ public/js/staffs.js
@@ -58,7 +58,10 @@
     let today = new Date();
     today.setHours(0,0,0,0); //0:00にセット
     today_ts = new Date(today).getTime();
-    const dateArray = createDateArray(today_ts);
+
+    //テスト用
+    const test_today = today_ts + 24*60*60*1000;
+    const dateArray = createDateArray(test_today);
 
     let index = {
       num
