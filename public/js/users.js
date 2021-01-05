e5b0bc67a827b7cb74b62cfd4d7404be8b79761e
kotrin-co <46315612+kotrin-co@users.noreply.github.com>
2021-01-05 17:57:08 +0900

    next reservation

--- public/js/users.js
+++ public/js/users.js
@@ -2,7 +2,20 @@
     const API_URL = 'https://linebot-schedule.herokuapp.com/api/';
     const HEADERS = ['ID','名前','登録日','次回予約','来店回数'];
     const CLASSES = ['row-id','row-name','row-resist','row-nextrev','row-visit'];
-    
+    const MENU = [
+        {
+          menu: 'タイ式（ストレッチ）',
+          timeAndPrice: [[30,3000],[45,4000],[60,5000],[90,7000],[120,9000]]
+        },
+        {
+          menu: 'タイ式（アロマ）',
+          timeAndPrice: [[45,5000],[60,7000],[90,9000],[120,12000]]
+        },
+        {
+          menu: '足つぼマッサージ',
+          timeAndPrice: [[30,3000],[60,5000]]
+        }
+      ]
     // div要素の取得
     const divElement = document.getElementById('usersPage');
 
@@ -65,7 +78,7 @@
             const now = new Date().getTime();
 
             // data.reservationsからdata.usersのline_uidが一致するもの、かつ現在時刻より先の予約データのみを抽出
-            let nextReservationDate = '';
+            let nextReservationData = '';
             STAFFS.forEach((name,index)=>{
                 const revData = data.reservations[index].filter(revObj1=>{
                     return usersObj.line_uid === revObj1.line_uid;
@@ -74,7 +87,7 @@
                 });
                 console.log('revData:',revData);
                 // revData.starttimeを日時文字列へ変換する
-                nextReservationDate = (revData.length) ? timeConversion(parseInt(revData[0].starttime),1) : '予約なし';
+                nextReservationData = (revData.length) ? nextDisplay(revData[0]) : '予約なし';
             });
 
             // usersObj.timestampを日時文字列へ変換する
@@ -85,7 +98,7 @@
                 usersObj.id,
                 usersObj.display_name,
                 resistrationDate,
-                nextReservationDate,
+                nextReservationData,
                 usersObj.visits
             ]);
 
@@ -142,6 +155,25 @@
         }else{
             return `${y}/${m}/${d} ${h}:${i}`
         }
+    }
+
+    const nextDisplay = (data) => {
+        const timestamp = data.starttime;
+        const date = new Date(timestamp);
+        const y = date.getFullYear();
+        const m = ("0" + (date.getMonth()+1)).slice(-2);
+        const d = ("0" + date.getDate()).slice(-2);
+        const h = ("0" + date.getHours()).slice(-2);
+        const i = ("0" + date.getMinutes()).slice(-2);
+        const nextData = {
+            nextDate: `${y}/${m}/${d} ${h}:${i}`,
+            menu: MENU[data.menu].menu,
+            treattime: MENU[data.menu].timeAndPrice[data.treattime][0],
+            staff: data.staff
+        }
+        console.log('nextData',nextData);
+        const nextHtml = `<span>${nextData.nextDate}</span><br><span>${nextData.menu}</span><br><span>${nextData.treattime}分</span><br><span>スタッフ：${nextData.staff}</span><br>`;
+        return nextHtml;
     }
 
 })();
