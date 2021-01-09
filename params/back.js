b5ca82ea8813205219a88280eb8bbe0b7e2dcee3
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2021-01-09 15:35:22 +0900

    git push heroku master

--- params/back.js
+++ params/back.js
@@ -6,9 +6,9 @@
     REGULAR_CLOSE: [], //定休日（整数、配列型式で入力）
     FUTURE_LIMIT: 3, //何日先まで予約可能か
     NUMBER_OF_SHIFTS: 7, //何日先のシフトまで入力するか
-    SHIFTS_LEFT: 7,　//南日前までのシフトを残すか
+    SHIFTS_LEFT: 7,　//何日前までのシフトを残すか
     PASSWORD: 'パスワード', //管理画面用URL
-    ADMIN_EMAIL_FROM: 'kentaro523@gmail.com', //Gメールの送り元(id,secret,refreshが登録されたもの)
+    ADMIN_EMAIL_FROM: 'noball99@gmail.com', //Gメールの送り元(id,secret,refreshが登録されたもの)
     ADMIN_EMAIL_TO: 'waruemon.xyz@gmail.com', //Gメールの送り先
     MENU: [
             {
