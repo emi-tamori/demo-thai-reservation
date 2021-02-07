ca37889c1f702b7fd1433e3e0bab5534d33f6943
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2021-02-07 14:36:20 +0900

    git push heroku master

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,40 @@ window.onload = () => {
     //LIFFで立ち上げているかどうかの判定
     if(liff.isInClient()){
         divElement.innerHTML='これはLIFF画面です'
+        //LIFF初期化
+        liff.init({
+            liffId: myLiffId
+        })
+        .then(()=>{
+            //idトークンによるスタッフ情報の取得
+            const idToken = liff.getIDToken();
+            const jsonData = JSON.stringify({
+              id_token: idToken
+            });
+            
+            fetch('/api',{
+              method: 'POST',
+              headers: {
+                'Content-Type':'application/json'
+              },
+              body: jsonData,
+              creadentials: 'same-origin'
+            })
+            .then(res=>{
+              //ここにレスポンス返ってくる
+            })
+            .catch(e=>console.log(e));
+            
+            //プロフィール情報の取得
+            liff.getProfile()
+              .then(profile=>{
+                const name = profile.displayNam;
+                const lineId = profile.userId;
+                const staff = profile.staff;
+                divElement.innerHTML=`あなたの名前は${name}です。LINE IDは${lineId}です。`;
+              });
+          })
+
     }else{
         divElement.innerHTML='これはLIFF画面じゃありません'
     }
