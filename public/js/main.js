c85a4de4c1f183c682bf0479fe17d1aa4f2ad6be
田盛絵美 <tamoriemi@tamorieminoMacBook-Pro.local>
2021-02-12 16:51:49 +0900

    git push heroku master

--- public/js/main.js
+++ public/js/main.js
@@ -24,7 +24,7 @@ window.onload = () => {
               body: jsonData,
               creadentials: 'same-origin'
             })
-            .then(res=>{
+            .then(response=>{
               //ここにレスポンス返ってくる
               const staff = response.staff;
               if(staff == true){
